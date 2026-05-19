463694a5f378061998f569a4c71e6244d58f25dc
KaioKraus <kaio_kraus@estudante.sesisenai.org.br>
2026-05-19 11:46:28 -0300

    add - atividade 4 js

---
+++ scripts/js4.js
@@ -0,0 +1,13 @@
+function ordenar(){
+
+    let numeros = [];
+
+    for (let i = 0; i < 4; i++) {
+        numeros.push(Number(prompt("Digite um número:")));
+    }
+
+    numeros.sort((a,b) =>b - a);
+
+    alert("Ordem decrescente:"+numeros);
+
+}

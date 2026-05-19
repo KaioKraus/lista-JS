21eb587dab8c22eb65f210a18faca23f7f953a01
KaioKraus <kaio_kraus@estudante.sesisenai.org.br>
2026-05-19 11:43:46 -0300

    add - atividade 3 js

---
+++ scripts/js3.js
@@ -0,0 +1,31 @@
+function balancoFinanceiro(){
+
+    let mes=1;
+    let soma_ganho=0;
+    let soma_gasto=0;
+    let saldo=0;
+
+    for(let i=1; i<=12; i++){
+        let ganho=parseFloat(prompt("Informe o ganho bruto do mês "+i));
+        let gasto=parseFloat(prompt("Informe o gasto do mês "+i));
+        soma_ganho+=ganho;
+        soma_gasto+=gasto;
+        
+    }
+
+    saldo=soma_ganho-soma_gasto;
+
+    alert(
+    "Ganho bruto anual: R$ " + soma_ganho + "\n" +
+    "Gasto anual: R$ " + soma_gasto + "\n" +
+    "Saldo financeiro anual: R$ " + saldo);
+
+    if(saldo>0){
+        alert("Lucro!");
+    }else if(saldo<0){
+        alert("Prejuízo!");
+    }else{
+        alert("Não sobrou nada para o betinha!");
+    }
+
+}

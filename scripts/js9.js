c77527f336e0b99333aee2d28b6196e5c10ef921
KaioKraus <kaio_kraus@estudante.sesisenai.org.br>
2026-05-19 11:51:17 -0300

    add - atividade 9 js

---
+++ scripts/js9.js
@@ -0,0 +1,12 @@
+function maiormenor(){
+
+    let numero=parseInt(prompt("Insira um número"));
+    let numero2=parseInt(prompt("Insira outro número"));
+
+    if(numero>numero2){
+        console.log(numero+", "+ numero2);
+    }else{
+        console.log(numero2+", "+ numero);
+    }
+
+}
